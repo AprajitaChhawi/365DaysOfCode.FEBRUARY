f7e5ad3dcbbe7fe8b88637cc07d1e4e1ea6ebdf2
AprajitaChhawi <achhawip@gmail.com>
2021-02-07 22:24:39 +0530

    Create Day 3 convert string to lower.py

---
+++ Day 3 convert string to lower.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def toLower (ob, S):
+        return S.lower()
+        # code here 
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        S = input()
+        
+        ob = Solution()
+        print(ob.toLower(S))
+# } Driver Code Ends
